edf8dbfe25c268361ce6e6c844bf2612f2ff1f33
roshan-980 <playerpro9800@gmail.com>
2025-12-20 22:55:32 +0530

    Done some Frontend work, added Defer (keyword) to automatically run js file everytime

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+console.log("Script loaded successfully");
+async function fetchNews() {
+    console.log(" i am from fetchNews function");
+    console.log(" i am working auto matically");
+    }
+fetchNews();
+console.log("Loaded at", Date.now());
